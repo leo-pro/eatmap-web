711d2b5945f5e77c347a10aae66129f241428c8b
Leonardo Alves <leoroberto.alves@hotmail.com>
2026-03-11 01:48:07 -0300

    chore: config vercel speed insights

--- src/app/providers/app-provider.tsx
+++ src/app/providers/app-provider.tsx
@@ -1,12 +1,13 @@
-import { RouterProvider } from '@tanstack/react-router'
-import { QueryProvider } from '@/app/providers/query-provider'
-import { router } from '@/app/router'
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryProvider } from "@/app/providers/query-provider";
+import { router } from "@/app/router";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export function AppProvider() {
   return (
     <QueryProvider>
       <RouterProvider router={router} />
+      <SpeedInsights />
     </QueryProvider>
-  )
+  );
 }
-
